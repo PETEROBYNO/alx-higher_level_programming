f49629597941697826b332b17cdade83040cbd32
PETEROBYNO <peterobinna67@gmail.com>
2024-01-10 22:27:47 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-def uniq_add(my_list=[]):
-    uniq_set = set()
-    total = 0
-    for num in my_list:
-        if num not in uniq_set:
-            uniq_set.add(num)
-            total += num
+def common_elements(set_1, set_2):
 
-    return total
+    common_set = {x for x in set_1 if x in set_2}
+
+    return common_set
