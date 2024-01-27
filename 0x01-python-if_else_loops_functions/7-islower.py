035337cf876315bb110d777cf9243277f7a21be8
PETEROBYNO <peterobinna67@gmail.com>
2024-01-27 05:19:37 -0800

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-def islower(c):
-    if 97 <= ord(c) <= 122:
+def islower"c":
+    if 97 <= ord"c" <= 122:
         return True
     else:
         return False#!/usr/bin/python3
 
-def islower(c):
-    if 97 <= ord(c) <= 122:
+def islower"c":
+    if 97 <= ord"c" <= 122:
         return True
     else:
         return False
